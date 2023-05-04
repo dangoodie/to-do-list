73770cbe9724b0cf25dfaff52efe35fef2fbbcd4
dangoodie <dan.gooden.dev@gmail.com>
2023-05-04 16:10:41 +1000

    setting up project cards

--- src/components/Main/MainContainer.js
+++ src/components/Main/MainContainer.js
@@ -1,9 +1,39 @@
 import Greeting from "./Greeting";
+import ProjectCard from "./ProjectCard.Js";
+import Project from "../../classes/Project";
 
 const MainContainer = () => {
   const container = document.createElement("div");
   container.className = "main-container";
+
   container.appendChild(Greeting());
+
+  const projectContainer = document.createElement("div");
+  projectContainer.className = "flex flex-row overflow-auto";
+
+  const projectA = new Project(
+    "Project A",
+    "A description of some sort",
+    "yellow"
+  );
+
+  const projectB = new Project(
+    "Project B",
+    "A description of some sort",
+    "purple"
+  );
+
+  const projectC = new Project(
+    "Project C",
+    "A description of some sort",
+    "cyan"
+  );
+
+  projectContainer.appendChild(ProjectCard(projectA));
+  projectContainer.appendChild(ProjectCard(projectB));
+  projectContainer.appendChild(ProjectCard(projectC));
+
+  container.appendChild(projectContainer)
 
   return container;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "space-mono": ["'Space Mono'", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
