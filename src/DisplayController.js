ee741597234ed8000a7ee74d0af0ce7aa20f543d
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-09-30 14:24:54 +1000

    Removed extra event listeners from the code

--- src/DisplayController.js
+++ src/DisplayController.js
@@ -7,30 +7,24 @@ const DisplayController = (props) => {
   const { projects } = props;
   const contentDiv = document.querySelector("#content");
 
-  // Initial build on load
   function updateDisplay() {
     contentDiv.innerHTML = "";
     contentDiv.appendChild(SideBar({ projects }));
     contentDiv.appendChild(MainContainer({ projects }));
     contentDiv.appendChild(Modal());
-    
+
     const newProjectBtn = document.querySelector(".add-project");
 
     // Handles new project button
     function handleAddBtn(event) {
       event.preventDefault();
-      // Remove event listener to prevent multiple triggers
-      newProjectBtn.removeEventListener("click", handleAddBtn);
-
+      
       const modalBg = document.querySelector(".modal-bg");
       modalBg.classList.add("show");
 
       const modalClose = document.querySelector(".close-btn");
       modalClose.addEventListener("click", () => {
         modalBg.classList.remove("show");
-
-        // Add event listener back to new project button
-        newProjectBtn.addEventListener("click", handleAddBtn);
       });
     }
     newProjectBtn.addEventListener("click", handleAddBtn);
@@ -50,8 +44,7 @@ const DisplayController = (props) => {
       projects.push(newProject);
       updateDisplay();
 
-      // Add event listener back to new project button
-      newProjectBtn.addEventListener("click", handleAddBtn);
+
     });
   }
   updateDisplay();
